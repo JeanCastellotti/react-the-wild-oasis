815dde5594a48f412ef979f77ad3bcd2ecf69fe1
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-06-27 18:24:39 +0200

    update style

--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
-  padding: 1rem 4.8rem 6.4rem;
+  padding: 4rem 4.8rem 6.4rem;
 `
 
 const StyledLayout = styled.div`
